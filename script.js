60c1e9213146065a9888ab99a9bc5c43eb7cced0
Melissa <melissapotter.student@careerdevs.com>
2019-04-02 19:37:51 -0400

    need to display results

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 function handleSubmit(){
   const xhttp = new XMLHttpRequest();
   const submitValue = document.getElementById("searchField").value;
-  const url = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search=" + submitValue + "&format=json&callback=?";
+  const url = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch='" + submitValue + "'";
+  xhttp.open("GET", url, true);
+  xhttp.responseType = 'text';
   
-  document.getElementById("results").innerHTML = this.responseText;
-  
-    xhttp.open("GET", url, true);
-    xhttp.send()
-    console.log(this.responseText);
+  xhttp.onload = function () {
+    	if (xhttp.status >= 200 && xhttp.status < 300) {
+    	var data = JSON.parse(xhttp.responseText);
+		console.log (data);
+		xhttp.responseText.forEach(function (results) {
+		var title = results.query.pages[0].title;
+		console.log(title);
+	});
+    }
+  };
+        //     //for troubleshooting purposes
+        //     console.log(xhttp.responseText);
+        //     console.log(url);
+        // //displays JSON in front end
+	       // document.getElementById("results").innerHTML = xhttp.responseText;
+	xhttp.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    xhttp.send();
 }
+    
+
+    
+
+    
+
+        
+
