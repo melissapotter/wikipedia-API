8b02796ff2e42e3abac7d5320d52eb8d09814896
Melissa <melissapotter.student@careerdevs.com>
2019-04-01 21:17:42 -0400

    cors error, undefined data

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function handleSubmit(){
   const xhttp = new XMLHttpRequest();
   const submitValue = document.getElementById("searchField").value;
-  var url = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + submitValue + "&utf8=&format=json";
+  const url = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search=" + submitValue + "&format=json&callback=?";
+  
+  document.getElementById("results").innerHTML = this.responseText;
   
     xhttp.open("GET", url, true);
-    xhttp.send();
-    console.log(xhttp.responseText);
+    xhttp.send()
+    console.log(this.responseText);
 }
