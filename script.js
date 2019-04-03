33f3a0d88cf0d05e3c8b42b30991467dc9c38e14
Melissa <melissapotter.student@careerdevs.com>
2019-04-02 22:58:03 -0400

    need to display more than one result

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-function handleSubmit(){
+function handleSubmit() {
   const xhttp = new XMLHttpRequest();
   const submitValue = document.getElementById("searchField").value;
   const url = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch='" + submitValue + "'";
+  
   xhttp.open("GET", url, true);
   xhttp.responseType = 'text';
-  
-  xhttp.onload = function () {
-    	if (xhttp.status >= 200 && xhttp.status < 300) {
-    	var data = JSON.parse(xhttp.responseText);
-		console.log (data);
-		xhttp.responseText.forEach(function (results) {
-		var title = results.query.pages[0].title;
-		console.log(title);
-	});
+
+  xhttp.onload = function() {
+    if (xhttp.status >= 200 && xhttp.status < 300) {
+      var data = JSON.parse(xhttp.responseText);
+      // console.log(data);
+      Object.keys(data).forEach(function(results) {
+        var arr = data[results].pages;
+        // console.log(arr);
+      for (var i in arr) {
+        document.getElementById("results").innerHTML = (arr[i].title);
+        console.log(arr[i].title);
+        
+    }
+
+    
+        //will print the array belongs to each property.
+
+      });
     }
   };
-        //     //for troubleshooting purposes
-        //     console.log(xhttp.responseText);
-        //     console.log(url);
-        // //displays JSON in front end
-	       // document.getElementById("results").innerHTML = xhttp.responseText;
-	xhttp.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-    xhttp.send();
+  //     //for troubleshooting purposes
+  //     console.log(xhttp.responseText);
+  //     console.log(url);
+  // //displays JSON in front end
+  
+  xhttp.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+  xhttp.send();
 }
-    
-
-    
-
-    
-
-        
-
